1e09938834cc43ae3b706b5bc5196fe2c201db41
iamJxstin <130079630+iamJxstin@users.noreply.github.com>
2023-04-16 14:47:32 -0400

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,5 +1,5 @@
+-- JustinHub - SupportedGames loadstring
 -- Uses game.GameId to track all sub-places for the respective game
 getgenv().SupportedGames = {
-	[4371473343] = "https://raw.githubusercontent.com/iamJxstin/JustinHUB/main/BlueLockedLeague.lua",
---	[2377350136] = "SG2"
+	[4371473343] = 'https://raw.githubusercontent.com/iamJxstin/JustinHUB/main/BlueLockedLeague.lua',
 }
