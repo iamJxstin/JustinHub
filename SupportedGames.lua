d2c1c5d04c938be3e9d97cc3da1ce68b72c37053
iamJxstin <130079630+iamJxstin@users.noreply.github.com>
2023-04-15 23:03:00 -0400

    Add files via upload

---
+++ SupportedGames.lua
@@ -0,0 +1,5 @@
+-- Uses game.GameId to track all sub-places for the respective game
+getgenv().SupportedGames = {
+	[4371473343] = "https://raw.githubusercontent.com/iamJxstin/JustinHUB/main/BlueLockedLeague.lua",
+--	[2377350136] = "SG2"
+}
